cf13334faad58d7f306823b1faaafeecb83c82df
Cristian de Leon <cdlmoran@icloud.com>
2026-03-10 22:23:01 -0600

    Creacion de objetos - Herencia

--- main.cpp
+++ main.cpp
@@ -1,16 +1,62 @@
 #include <iostream>
+using namespace std;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+class Mascotas {
+private:
+    int Id_Mascotas; // atributos
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+public:
+    string Nombre; // atributos
+    string Color; // atributos
+    string Raza; // atributos
+    float Peso; // atributos
+    int Edad; // Atributos
+
+
+    void saltar() { //Metodo
+        cout << Nombre << " Salta";
     }
+};
+class Mascotas_Perros : public Mascotas {
+public:
+    void ladrar() {
+   cout << Nombre << "el perro esta ladrando";
+    }
+};
+class Mascotas_Gatos : public Mascotas {
+public:
+    void ladrar() {
+        cout << Nombre << "el perro esta ladrando";
+    }
+};
+int main () {
+    Mascotas mascotas1;
+    mascotas1.Nombre = "Rudolf";
+    cout << mascotas1.Nombre << endl;
+    mascotas1.Color = "azul";
+    cout << mascotas1.Color << endl;
+    mascotas1.Edad = 10;
+    cout << mascotas1.Edad << endl;
+    mascotas1.Peso = 7.5;
+    cout << mascotas1.Peso << endl;
+    mascotas1.Raza = "shitzu";
+    cout << mascotas1.Raza << endl;
+    mascotas1.saltar();
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    cout << endl << endl;
+
+    Mascotas_Perros mascotas2;
+    mascotas2.Nombre = "rufo";
+    cout << mascotas2.Nombre << endl;
+    mascotas2.Color = "verde";
+    cout << mascotas2.Color << endl;
+    mascotas2.Edad = 5;
+    cout << mascotas2.Edad << endl;
+    mascotas2.Peso = 8.5;
+    cout << mascotas2.Peso << endl;
+    mascotas2.Raza = "galez";
+    cout << mascotas2.Raza << endl;
+    mascotas2.ladrar();
+
+
 }
